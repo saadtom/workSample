732347ac96bc197098d6f15687b5650d98a45f3e
Tommy Tomaye <tommytomaye2018@gmail.com>
2018-08-19 11:24:00 -0700

    new changes

--- src/app/config.service.ts
+++ src/app/config.service.ts
@@ -2,7 +2,8 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class ConfigService{
-            getHeading(){
-                return 'Weather-Combo-Movie App';
-            }
+
+     getHeading(){
+        return 'Weather-Combo-Movie App';
+     }
 }

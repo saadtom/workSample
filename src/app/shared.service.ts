ff308694d8cc7f251bc36dba90ae2aa5d1c23240
Tommy Tomaye <tommytomaye2018@gmail.com>
2018-08-19 11:22:16 -0700

    new changes

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -5,18 +5,17 @@ import { Observable } from "rxjs";
 
 @Injectable()
 export class SharedService {
-  //weatherURL1 = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22";
+  // Let's define the api url
   weatherURL1="http://api.apixu.com/v1/current.json?key=0e70beed6cfb4e4dbb6153029172608 &q=";
-  //weatherURL3 = "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
-  weatherURL2="";
   movieURL1 = "http://api.themoviedb.org/3/search/movie?api_key=5aa2924101cb102ea40ead255bcadd62&query=";
   movieURL2="http://api.themoviedb.org/3/movie/";
   movieURL3= "?api_key=5aa2924101cb102ea40ead255bcadd62";
   movieURL4="/images";
-  total_ReqsMade: number = 0; 
-  
+  total_ReqsMade: number = 0;
+
   constructor(private _http: Http) { }
 
+  // Fetch the data from the url 
   findWeather(city) {
     this.total_ReqsMade = this.total_ReqsMade + 1;
     return this._http.get(this.weatherURL1 + city  + this.weatherURL2)
@@ -40,7 +39,7 @@ export class SharedService {
   }
 
   getMovie(movie_id){
-    return this._http.get(this.movieURL2 + movie_id + this.movieURL3) 
+    return this._http.get(this.movieURL2 + movie_id + this.movieURL3)
     .map(response => {
       {
         return response.json()
@@ -50,7 +49,7 @@ export class SharedService {
   }
 
   getImage(movie_id){
-    return this._http.get(this.movieURL2 + movie_id + this.movieURL4 + this.movieURL3) 
+    return this._http.get(this.movieURL2 + movie_id + this.movieURL4 + this.movieURL3)
     .map(response => {
       {
         return response.json()
